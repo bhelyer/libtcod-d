3026f6dbd67bdc7c53ff43e403bcdd2994aa2edb
Bernard Helyer <b.helyer@gmail.com>
2010-05-22 19:34:27 +1200

    Added simple sample.

---
+++ simple.d
@@ -0,0 +1,16 @@
+module simple;
+
+import c.tcod.all;
+
+
+void main()
+{
+    TCOD_console_init_root(80, 50, "Hello, world.", false);
+    
+    while(!TCOD_console_is_window_closed()) {
+        TCOD_console_print_left(null, 0, 0, TCOD_BKGND_NONE, "Hello, world.");
+        TCOD_console_flush();
+        auto k = TCOD_console_check_for_keypress(TCOD_KEY_PRESSED);
+    }
+}
+
